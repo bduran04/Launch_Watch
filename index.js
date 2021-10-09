865c09a5a5b374ad570e8ceb75b63672c9296cfb
Belle Duran <bduran04@gmail.com>
2021-10-09 15:25:42 -0500

    refactored the code to return json object; WIP to ensure correct data returns

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-const inquirer = require('inquirer');
+import inquirer from 'inquirer'
 const launchpath = 'https://lldev.thespacedevs.com/2.2.0/launch/';
+import fetch from 'node-fetch';
 
-//make the API call with the start & end date
-//use a package to format the data for the dates
 //get the results & process them
 // map over the array of objects & guarantee whether something is not a broken link
 //maybe there is a package that
@@ -12,34 +11,40 @@ const launchpath = 'https://lldev.thespacedevs.com/2.2.0/launch/';
 
 const questions = [{
     type: 'input',
-    name: 'first_date',
-    message: 'What is the initial date of the launch?'
+    name: 'start_date',
+    message: 'What is the initial date of the launch(format: YYYY-MM-Do)?'
 }, {
     type: 'input',
-    name: 'last_date',
-    message: 'What is the last date of the launch?'
+    name: 'end_date',
+    message: 'What is the last date of the launch(format: YYYY-MM-Do)?'
 }];
 
-//format the date into 2021-10-31T03:00:21Z 
-async function formatDate(data) {
-   await data.format('MMMM Do YYYY, h:mm:ss a')
-}
 //once the questions are asked, insert the answers in this & fetch the information from the API https://lldev.thespacedevs.com/2.2.0/launch/?is_crewed=true&window_start=2020-10-31T03:00:21Z&window_end=2021-10-31T03:00:21Z 
 
-async function retrieveURLParams(data) {
-    await fetch(`${launchpath}?is_crewed=true&window_start=${first_date}&window_end=${last_date}`)
-        .then(function (response) { return response.json() })
+async function retrieveURLParams(answers) {
+    try {
+        await fetch(`${launchpath}?is_crewed=true&window_start=${answers.start_date}T00:00:00.000Z&window_end=${answers.end_date}T00:00:00.000Z`, 
+        {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json',
+              }
+        }).then((response) => {
+            console.log(response)
+        })
+
+    } catch (error) {
+        console.error(error);
+    }
 }
 
 function processApp() {
     inquirer
         .prompt(questions)
         .then((answers) => {
-            formatDate();
+            retrieveURLParams(answers)
             console.log(answers)
         });
 }
 
 processApp();
-
-
