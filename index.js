98f7abcdfbc295e079ca7ee6e48edd6a684b9d1a
Belle Duran <bduran04@gmail.com>
2021-10-09 21:25:25 -0500

    commented the next steps to complete the application

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
-import inquirer from 'inquirer'
-const launchpath = 'https://lldev.thespacedevs.com/2.2.0/launch/';
+import inquirer from 'inquirer';
 import fetch from 'node-fetch';
+const launchpath = 'https://lldev.thespacedevs.com/2.2.0/launch/';
 
-//get the results & process them
-// map over the array of objects & guarantee whether something is not a broken link
-//maybe there is a package that
+//map over the results by processing individual result id's & ensure that status.name = launch successful 
+//want to display name, window start, window end, description, all vidURLs.url
 //want to populates the list of results
-
-//create an if statement indicating that if results.status.name === Launch Successful then useState and provide the results of the 
 
 const questions = [{
     type: 'input',
@@ -23,17 +20,15 @@ const questions = [{
     message: 'What is the location of the launch?'
 }];
 
-//once the questions are asked, insert the answers in this & fetch the information from the API https://lldev.thespacedevs.com/2.2.0/launch/?window_start__gt=1961-05-05T00%3A00%3A00Z&window_end__gt=1961-05-05T00%3A00%3A00Z&is_crewed=true&search=USA
-
 async function retrieveURLParams(answers) {
     try {
         await fetch(`${launchpath}?window_start__gt=${answers.start_date}T00%3A00%3A00Z&window_end__gt=${answers.end_date}T00%3A00%3A00Z&is_crewed=true&search=${answers.location}`,
-        {
-            method: "GET",
-            headers: {
-                'Content-Type': 'application/json',
-              }
-        }).then((response) => response.json())
+            {
+                method: "GET",
+                headers: {
+                    'Content-Type': 'application/json',
+                }
+            }).then((response) => response.json())
             .then(data => console.log(data))
 
     } catch (error) {
@@ -46,7 +41,6 @@ function processApp() {
         .prompt(questions)
         .then((answers) => {
             retrieveURLParams(answers)
-            console.log(answers)
         });
 }
 
